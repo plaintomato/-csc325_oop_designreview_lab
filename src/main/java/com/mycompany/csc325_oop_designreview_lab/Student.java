add6ce2df862683ee70fe9bb71e3f106366e3fd5
soblab <soblab@Whit209-17.it.farmingdale.edu>
2022-03-01 14:58:08 -0500

    initial commit, completed up to TODO #9

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -9,13 +9,39 @@ package com.mycompany.csc325_oop_designreview_lab;
  *
  * @author MoaathAlrajab
  */
-public class Student {
-	// ToDo 1: Make this class a child of Human
-	
-	// ToDo 2: Fix the resulting errors
-	
-	// ToDo 3: Add a field for GPA and create setter and getter
-	
-	// ToDo 4: Add comments to your code
-
+public class Student extends Human {
+    private double gpa;
+    private int credits;
+    
+    public Student(String name, int age, int credits){
+        super(name, age);
+        if(credits < 0) throw new IllegalArgumentException("Credit requirements for class standing not met");
+        else this.credits = credits;
+    }
+    
+    @Override
+    public String getAddress(){
+        return address;
+    }
+    
+    @Override
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public double getGPA(){
+        return gpa;
+    }
+    
+    public void setGPA(double gpa) {
+        this.gpa = gpa;
+    }
+    
+    public int getCredits(){
+        return credits;
+    }
+    
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
 }
